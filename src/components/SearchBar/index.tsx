bb775b8da9655588dcbad07865c3b9681ec011de
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-07-29 12:32:39 -0300

    feat: add onChange props

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,6 +1,11 @@
 import { IconSearch } from "@/assets/icons/IconSearch";
+import { ChangeEvent, FC } from "react";
 
-export const SearchBar = () => {
+interface ISearchBar {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const SearchBar: FC<ISearchBar> = ({ onChange }) => {
   return (
     <div className="relative bg-slate-50 w-96 px-4 py-2 rounded">
       <IconSearch className="absolute left-2 h-6 fill-gray-500" />
@@ -8,7 +13,8 @@ export const SearchBar = () => {
         type="text"
         className="ml-6 bg-slate-50 w-11/12 outline-none"
         placeholder="Busque um livro aqui..."
+        onChange={onChange}
       />
     </div>
   );
-}
+};
