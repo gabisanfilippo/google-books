dd01d1e21faed8b0a6f101f7321fd06d73b85ac0
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-07-31 09:30:27 -0300

    feat: head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>BOOKSTORE</title>
+        <meta name="description" content="Bookstore page" />
+        <meta name="keywords" content="Book, Books, Google Books, Library" />
+        <meta name="author" content="Gabrielle Coelho Sanfilippo" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
