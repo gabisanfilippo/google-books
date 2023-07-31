41cc162c36796c71f29c5ca56763392e2b190161
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-07-31 09:33:52 -0300

    fix: height bug

--- src/components/BoxText/index.tsx
+++ src/components/BoxText/index.tsx
@@ -7,9 +7,9 @@ interface IBoxText {
 export const BoxText: FC<IBoxText> = ({ children }) => {
   return (
     <div
-      className={`bg-gray-600 rounded-3xl relative mt-2 ml-2 max-w-lg py-4 px-8 w-full`}
+      className={`bg-gray-600 rounded-3xl relative mt-2 ml-2 max-w-lg py-4 px-8 w-full h-max`}
     >
-      <div className="invisible">{children}</div>
+      <div className="invisible h-max">{children}</div>
       <div className="bg-white rounded-3xl border-2 border-gray-950 border-dashed py-4 px-8 max-w-lg w-full absolute bottom-2 right-2">
         {children}
       </div>
