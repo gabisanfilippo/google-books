63453e66a41f82054abb6486358e848048a9917c
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-07-30 21:33:42 -0300

    feat: redirect to details page

--- src/components/BookItem/index.tsx
+++ src/components/BookItem/index.tsx
@@ -1,14 +1,17 @@
 import { Item } from "@/services/GET/getVolumes";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { FC } from "react";
 
 interface IBookItem {
   info: Item;
 }
 
-export const BookItem: FC<IBookItem> = ({info}) => {
+export const BookItem: FC<IBookItem> = ({ info }) => {
+  const router = useRouter()
+
   return (
-    <article className="bg-slate-50 p-4 rounded flex flex-col gap-8">
+    <article className="bg-slate-50 p-4 rounded flex flex-col gap-8 cursor-pointer" onClick={() => router.push(`/${info.id}`)}>
       <div className="w-120 h-200">
         {info.volumeInfo.imageLinks ? (
           <Image
@@ -26,7 +29,9 @@ export const BookItem: FC<IBookItem> = ({info}) => {
         <p className="text-xs">
           Por:{" "}
           {info?.volumeInfo?.authors?.map((author, index) =>
-            index  === info?.volumeInfo?.authors.length - 1 ? author : author + ", "
+            index === info?.volumeInfo?.authors.length - 1
+              ? author
+              : author + ", "
           )}
         </p>
       </div>
