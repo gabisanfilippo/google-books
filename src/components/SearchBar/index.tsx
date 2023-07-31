6ebb0441224ee7d3303e2df786e94cd6f83a2d23
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-07-31 08:36:32 -0300

    feat: responsiveness

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -7,11 +7,11 @@ interface ISearchBar {
 
 export const SearchBar: FC<ISearchBar> = ({ onChange }) => {
   return (
-    <div className="relative bg-amber-500 w-96 rounded-3xl h-10 ml-2">
+    <div className="relative bg-amber-500 sm:w-96 w-11/12 rounded-3xl h-10 ml-2">
       <IconSearch className="absolute right-6 h-6 fill-gray-950 z-20" />
       <input
         type="text"
-        className="bg-white w-96 px-4 outline-none rounded-3xl absolute bottom-2 h-10 border-solid border-2 border-gray-950 right-2 z-10"
+        className="bg-white sm:w-96 w-full px-4 outline-none rounded-3xl absolute bottom-2 h-10 border-solid border-2 border-gray-950 right-2 z-10"
         placeholder="Type the name of book..."
         onChange={onChange}
       />
