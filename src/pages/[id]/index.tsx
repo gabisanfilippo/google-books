57906de568d957d881c10aa7e4e9f098c3aaf17a
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-07-31 07:20:18 -0300

    style: eslint disable

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -26,6 +26,7 @@ export default function BookDetails() {
 
   useEffect(() => {
     handleFetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id])
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ export default function Home() {
 
   useEffect(() => {
     handleFetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputValue]);
 
   return (
